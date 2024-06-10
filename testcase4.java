74b0d5604949f901f9b8f98e74d3958b12ded3a9
xikyno <1512522172@qq.com>
2024-06-10 16:05:13 +0800

    lab3

---
+++ testcase4.java
@@ -0,0 +1,16 @@
+import org.junit.Test;
+
+public class Lab1Test {
+
+    @Test
+    public void testReadFileAndCreateGraph() {
+        Lab1.readFileAndCreateGraph();
+    }
+    @Test
+    public void testCalcShortestPath() {
+
+        System.out.println(Lab1.calcShortestPath( null , "the"));
+    }
+
+
+}
